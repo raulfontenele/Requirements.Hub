0eb6d4bfa99ddc8ff815c1e5d6316efe2d994866
Raul <raulfontenele@edu.unifor.br>
2024-07-24 20:52:19 -0300

    Adicionada classe de Controle de projetos

---
+++ Requirements.Hub/Requirements.Hub.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Requirements.Hub.Application.UseCases.Gets;
+using Requirements.Hub.Application.UseCases.Sets;
+using Requirements.Hub.Communication.Request.Project;
+
+namespace Requirements.Hub.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddProject([FromBody] BaseProjectRequestJSON request)
+        {
+            try
+            {
+                AddProjectUseCase projectsUseCases = new AddProjectUseCase();
+
+                projectsUseCases.AddShortProject(request);
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest("Erro Inesperado");
+            }
+            
+        }
+        [HttpGet]
+        public IActionResult GetAllProjects()
+        {
+            try
+            {
+                GetProjectsUseCases projectsUseCases = new GetProjectsUseCases();
+
+                var projects = projectsUseCases.GetAllShortProjects();
+
+                return Ok(projects);
+            }
+            catch (Exception)
+            {
+                return BadRequest("Erro Inesperado");
+            }
+        }
+    }
+}

5327faab8ea842c8315f879b460650dc4f4c59b0
Raul Santana <raul.santana@eletraenergy.com>
2024-07-25 21:22:50 -0300

    Adicionada classe de Delete/DeleteRequirementsUseCase.cs

---
+++ Requirements.Hub/Requirements.Hub.Application/UseCases/Delete/DeleteRequirementsUseCase.cs
@@ -0,0 +1,26 @@
+﻿using Requirements.Hub.Communication.Response.Requirement;
+using Requirements.Hub.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Requirements.Hub.Application.UseCases.Delete
+{
+    public class DeleteRequirementsUseCase
+    {
+        public void DeleteAllRequirements() 
+        {
+            using (var context = new RequirementContext())
+            {
+
+                var requirements = context.GetAllFullRequirements();
+
+                foreach (var requirement in requirements)
+                    context.DeleteRequirement(requirement);
+
+            }
+        }
+    }
+}
